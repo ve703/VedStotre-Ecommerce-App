408abb606a00b32fb120df65a0ba3b56a3aad909
Vedant Akare <81575978+ve703@users.noreply.github.com>
2023-07-21 11:00:18 +0530

    Final Code
    Ecommerce MERN Stack App

---
+++ config/db.js
@@ -0,0 +1,15 @@
+import mongoose, { connect } from "mongoose";
+import colors from "colors";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `Connected To MongoDB Database ${conn.connection.host}`.bgMagenta.white
+    );
+  } catch (error) {
+    console.log(`Error in MongoDB ${error}`.bgRed.white);
+  }
+};
+
+export default connectDB;
